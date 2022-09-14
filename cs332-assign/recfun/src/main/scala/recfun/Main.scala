304cfc8f7249b76241463d3956b14ebf2a27d6a4
jyhong1 <wmbhoph1@gmail.com>
2022-09-14 17:47:41 +0900

    assn1 complete(recfun)

--- cs332-assign/recfun/src/main/scala/recfun/Main.scala
+++ cs332-assign/recfun/src/main/scala/recfun/Main.scala
@@ -14,15 +14,33 @@ object Main {
   /**
    * Exercise 1
    */
-  def pascal(c: Int, r: Int): Int = ???
+  def pascal(c: Int, r: Int): Int = {
+    if(c == 0 || c == r) 1
+    else pascal(c-1, r-1) + pascal(c, r-1)
+  }
 
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+  def balance(chars: List[Char]): Boolean = {
+    def checkBalance(chars: List[Char], count: Int): Boolean = {
+      if (chars.isEmpty) count == 0
+      else if (count < 0) false
+      else{
+        if (chars.head == '(') checkBalance(chars.tail, count + 1)
+        else if (chars.head == ')') checkBalance(chars.tail, count - 1)
+        else checkBalance(chars.tail, count)
+      }
+    }
+    checkBalance(chars, 0)
+  }
 
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = {
+    if(money == 0) 1
+    else if(coins.isEmpty || money < 0) 0
+    else countChange(money - coins.head, coins) + countChange(money, coins.tail)
+  }
 }
